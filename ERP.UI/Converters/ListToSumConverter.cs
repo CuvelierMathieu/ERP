9670b6dd66cefddfd483009646df67e224d71a73
Mathieu Cuvelier <mathieu@cuvelier.dev>
2021-07-13 17:51:45 +0200

    tests in progress

--- ERP.UI/Converters/ListToSumConverter.cs
+++ ERP.UI/Converters/ListToSumConverter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Globalization;
+using System.Linq;
 using System.Windows.Data;
 
 namespace ERP.UI.Converters
@@ -8,7 +10,43 @@ namespace ERP.UI.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value is null)
+                return 0;
+
+            if (value is string
+                || value is not IEnumerable list)
+                throw new ArgumentException("Value is expected to be a IEnumerable");
+
+            double sum = 0;
+            IEnumerator enumerator = list.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                if (enumerator.Current is double currentDoubleValue)
+                    sum += currentDoubleValue;
+                else if (enumerator.Current is int currentIntValue)
+                    sum += currentIntValue;
+                else if (enumerator.Current is decimal currentDecimalValue)
+                    sum += double.Parse(currentDecimalValue.ToString());
+                else if (parameter is null)
+                    throw new ArgumentException("Current item type is not handled by converter and parameter is null");
+                else if (parameter is string stringParameter)
+                {
+                    if (string.IsNullOrWhiteSpace(stringParameter))
+                        throw new ArgumentException("Parameter is empty or white space");
+                }
+            }
+
+            if (targetType == typeof(double))
+                return sum;
+
+            if (targetType == typeof(decimal))
+                return decimal.Parse(sum.ToString());
+
+            if (targetType == typeof(int))
+                return (int)Math.Round(sum);
+
+            throw new NotImplementedException($"Converter is not handled to target type {targetType}");
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
---
+++ ERP.UITest/Converters/ListToSumConverterTest.cs
@@ -0,0 +1,202 @@
+﻿using ERP.UI.Converters;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace ERP.UITest.Converters
+{
+    [TestFixture]
+    public class ListToSumConverterTest
+    {
+        public static object[] DoubleList = new object[]
+        {
+            new List<double>
+            {
+                1.2,
+                3.4,
+                5.6,
+                7.89
+            },
+        };
+
+        public static object[] IntList = new object[]
+        {
+            new List<int>
+            {
+                1,
+                3,
+                5,
+                7
+            },
+        };
+
+        public static object[] DecimalList = new object[]
+        {
+            new List<decimal>
+            {
+                1.2m,
+                3.4m,
+                5.6m,
+                7.89m
+            },
+        };
+
+        private static readonly List<double> DefaultValue = (List<double>)DoubleList[0];
+        private static readonly Type DefaultTargetType = typeof(double);
+        private const string DefaultParameter = "";
+        private static readonly CultureInfo DefaultCulture = CultureInfo.InvariantCulture;
+
+        private ListToSumConverter _converter;
+
+        [SetUp]
+        public void InitializeConverter()
+        {
+            _converter = new();
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(1)]
+        [TestCase(true)]
+        public void ThrowsArgumentExceptionIfValueIsNotAIEnumerable(object value)
+        {
+            Assert.Throws<ArgumentException>(() => _converter.Convert(value, DefaultTargetType, DefaultParameter, DefaultCulture));
+        }
+
+        [Test]
+        public void ReturnsZeroIfValueIsNull()
+        {
+            object actual = _converter.Convert(null, DefaultTargetType, DefaultParameter, DefaultCulture);
+            double expected = 0;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(DoubleList))]
+        public void ReturnsExpectedSum(List<double> valuesToBeAdded)
+        {
+            double expected = valuesToBeAdded.Sum();
+            object actual = _converter.Convert(valuesToBeAdded, DefaultTargetType, DefaultParameter, DefaultCulture);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(IntList))]
+        public void ReturnsExpectedSum(List<int> valuesToBeAdded)
+        {
+            int expected = valuesToBeAdded.Sum();
+            object actual = _converter.Convert(valuesToBeAdded, DefaultTargetType, DefaultParameter, DefaultCulture);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(DecimalList))]
+        public void ReturnsExpectedSum(List<decimal> valuesToBeAdded)
+        {
+            decimal expected = valuesToBeAdded.Sum();
+            object actual = _converter.Convert(valuesToBeAdded, DefaultTargetType, DefaultParameter, DefaultCulture);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(DecimalList))]
+        public void ReturnsDoubleIfAsked(List<decimal> valuesToBeAdded)
+        {
+            object actual = _converter.Convert(valuesToBeAdded, typeof(double), DefaultParameter, DefaultCulture);
+
+            Assert.IsTrue(actual is double, $"Actual value type is {actual.GetType()}");
+        }
+
+        [Test]
+        [TestCaseSource(nameof(IntList))]
+        public void ReturnsDoubleIfAsked(List<int> valuesToBeAdded)
+        {
+            object actual = _converter.Convert(valuesToBeAdded, typeof(double), DefaultParameter, DefaultCulture);
+
+            Assert.IsTrue(actual is double, $"Actual value type is {actual.GetType()}");
+        }
+
+        [Test]
+        [TestCaseSource(nameof(DoubleList))]
+        public void ReturnsDecimalIfAsked(List<double> valuesToBeAdded)
+        {
+            object actual = _converter.Convert(valuesToBeAdded, typeof(decimal), DefaultParameter, DefaultCulture);
+
+            Assert.IsTrue(actual is decimal, $"Actual value type is {actual.GetType()}");
+        }
+
+        [Test]
+        [TestCaseSource(nameof(IntList))]
+        public void ReturnsDecimalIfAsked(List<int> valuesToBeAdded)
+        {
+            object actual = _converter.Convert(valuesToBeAdded, typeof(decimal), DefaultParameter, DefaultCulture);
+
+            Assert.IsTrue(actual is decimal, $"Actual value type is {actual.GetType()}");
+        }
+
+        [Test]
+        [TestCaseSource(nameof(DecimalList))]
+        public void ReturnsIntIfAsked(List<decimal> valuesToBeAdded)
+        {
+            object actual = _converter.Convert(valuesToBeAdded, typeof(int), DefaultParameter, DefaultCulture);
+
+            Assert.IsTrue(actual is int, $"Actual value type is {actual.GetType()}");
+        }
+
+        [Test]
+        [TestCaseSource(nameof(DoubleList))]
+        public void ReturnsIntIfAsked(List<double> valuesToBeAdded)
+        {
+            object actual = _converter.Convert(valuesToBeAdded, typeof(int), DefaultParameter, DefaultCulture);
+
+            Assert.IsTrue(actual is int, $"Actual value type is {actual.GetType()}");
+        }
+
+        [Test]
+        public void RoundsDownIfTargetTypeIsIntAndFirstDecimalDigitIsLesserThan5()
+        {
+            List<double> value = new() { 3.49 };
+            int expected = 3;
+
+            object actual = _converter.Convert(value, typeof(int), DefaultParameter, DefaultCulture);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void RoundsUpIfTargetTypeIsIntAndFirstDecimalDigitIs5OrGreater()
+        {
+            List<double> value = new() { 3.5 };
+            int expected = 4;
+
+            object actual = _converter.Convert(value, typeof(int), DefaultParameter, DefaultCulture);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("     ")]
+        public void ThrowAnArgumentExceptionIfValueIsAComplexObjectAndNoParameterWasGiven(string parameter)
+        {
+            List<ComplexObjectForTestingListToSumConverter> list = new()
+            {
+                new() { Value = 3.14 }
+            };
+
+            Assert.Throws<ArgumentException>(() => _converter.Convert(list, DefaultTargetType, parameter, DefaultCulture));
+        }
+    }
+
+    public class ComplexObjectForTestingListToSumConverter
+    {
+        public double Value { get; set; }
+    }
+}
