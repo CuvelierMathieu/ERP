b0501b0545a7ec428c60d748c42dd3b3c11bed23
Mathieu Cuvelier <mathieu@cuvelier.dev>
2021-07-19 19:34:03 +0200

    add DividerMultiConverter

---
+++ ERP.UI.Common/Converters/DividerMultiConverter.cs
@@ -0,0 +1,44 @@
+﻿using ERP.Common.Helpers.Types;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace ERP.UI.Common.Converters
+{
+    public class DividerMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values is null || !values.Any())
+                throw new ArgumentNullException(nameof(values));
+
+            if (values.Length != 2)
+                throw new ArgumentException("Converter can only convert exactly two values", nameof(values));
+
+            if (values.Any(v => v is null))
+                return null;
+
+            Type firstValueType = values[0].GetType();
+            Type secondValueType = values[1].GetType();
+
+            ITypeHandler firstTypeHandler = TypeHandlerBuilder.Build(firstValueType);
+            ITypeHandler secondTypeHandler = TypeHandlerBuilder.Build(secondValueType);
+
+            double firstValue = firstTypeHandler.ConvertToDouble(values[0]);
+            double secondValue = secondTypeHandler.ConvertToDouble(values[1]);
+
+            double result = firstValue / secondValue;
+
+            if (targetType == typeof(string))
+                return result.ToString();
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ERP.UI.CommonTest/Converters/DividerMultiConverterTest.cs
@@ -0,0 +1,71 @@
+﻿using ERP.UI.Common.Converters;
+using NUnit.Framework;
+using System;
+using System.Globalization;
+
+namespace ERP.UI.CommonTest.Converters
+{
+    [TestFixture]
+    public class DividerMultiConverterTest
+    {
+        private static readonly Type DefaultTargetType = typeof(double);
+        private static readonly object DefaultParameter = null;
+        private static readonly CultureInfo DefaultCulture = CultureInfo.InvariantCulture;
+
+        private DividerMultiConverter converter;
+
+        [SetUp]
+        public void InitializeConverter()
+        {
+            converter = new();
+        }
+
+        [Test]
+        public void ConvertANullArrayThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => converter.Convert(null, DefaultTargetType, DefaultParameter, DefaultCulture));
+        }
+
+        [Test]
+        public void ConvertAnEmptyArrayThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => converter.Convert(Array.Empty<object>(), DefaultTargetType, DefaultParameter, DefaultCulture));
+        }
+
+        [Test]
+        public void ConvertAnArrayNotContainingExactlyTwoValuesThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => converter.Convert(new object[3], DefaultTargetType, DefaultParameter, DefaultCulture));
+        }
+
+        private static object[] InputsAndExpectedResults()
+        {
+            return new object[]
+            {
+                (new object[] { 10, null }, (double?)null),
+                (new object[] { null, 2 }, (double?)null),
+                (new object[] { 10, 2 }, (double?)5.0),
+                (new object[] { 10.0, 2m }, (double?)5.0),
+                (new object[] { 12f, "4" }, (double?)3.0),
+                (new object[] { "5.0", "-2,0" }, (double?)-2.5),
+            };
+        }
+
+        [Test]
+        [TestCaseSource(nameof(InputsAndExpectedResults))]
+        public void ReturnsExpectedDivisionResult((object[] values, double? expected) tuple)
+        {
+            object actual = converter.Convert(tuple.values, DefaultTargetType, DefaultParameter, DefaultCulture);
+
+            Assert.AreEqual(tuple.expected, actual);
+        }
+
+        [Test]
+        public void ReturnsAStringIfAsked()
+        {
+            object actual = converter.Convert(new object[2] { 80, 8 }, typeof(string), DefaultParameter, DefaultCulture);
+
+            Assert.True(actual is string);
+        }
+    }
+}
