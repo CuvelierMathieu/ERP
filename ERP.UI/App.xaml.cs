401cd833f32942ba23497dd06c27019f9b14a192
Mathieu Cuvelier <mathieu@cuvelier.dev>
2021-10-26 20:22:55 +0200

    add unhandled exception dispatcher

--- ERP.UI/App.xaml.cs
+++ ERP.UI/App.xaml.cs
@@ -13,5 +13,15 @@ namespace ERP.UI
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, e.Exception.GetType().Name);
+            e.Handled = true;
+        }
     }
 }
